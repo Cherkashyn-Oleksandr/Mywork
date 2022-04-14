c5db997158b4c51432acd239c2ccd62ca0bfbe72
Cherkashyn-Oleksandr <103587031+Cherkashyn-Oleksandr@users.noreply.github.com>
2022-04-14 23:54:36 +0300

    задание mvc
    mvc Task

---
+++ FirstTask/FirstTask/Controllers/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace FirstTask.Controllers
+{
+    public class HelloWorld : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Welcome(string name, int NumTimes = 1)
+        {
+            ViewData["Messages"] = "Hello " + name;
+            ViewData["NumTimes"] = NumTimes;
+            return View();
+        }
+    }
+}
---
+++ FirstTask/FirstTask/Data/FirstTaskContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FirstTask.Models;
+
+namespace FirstTask.Data
+{
+    public class FirstTaskContext : DbContext
+    {
+        public FirstTaskContext (DbContextOptions<FirstTaskContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FirstTask.Models.Games> Games { get; set; }
+    }
+}
