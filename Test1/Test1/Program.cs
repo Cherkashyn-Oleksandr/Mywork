d1afc9461ced45b597b7770ffb0506003f035473
Cherkashyn-Oleksandr <Oleks.cherkashyn@gmail.com>
2022-05-06 10:37:49 +0300

    Testing api

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml;
+
+namespace Test1
+{
+    /// <summary>
+    /// Summary description for Class1.
+    /// </summary>
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+          
+            String URLString = "https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php?lang=eng";
+            var weatherlist = new List<Weather>();
+            using (XmlTextReader xmlReader = new XmlTextReader(URLString))
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(xmlReader);
+                var parentNode = xmlDocument.SelectNodes("//forecasts//forecast//day//place");
+                var dayNode = xmlDocument.SelectNodes("//forecasts//forecast//night//place");
+                foreach (XmlNode node in parentNode)
+                {
+                    Weather weather;
+                    weather = new Weather();
+                    weather.name = node["name"].InnerText;
+                    weather.phenomenon = node["phenomenon"].InnerText;
+                    weather.tempmax = node["tempmax"].InnerText;
+                    
+                    
+
+                        weatherlist.Add(weather);
+                        Console.WriteLine(weather.name); 
+                        Console.WriteLine(weather.phenomenon);
+                        
+                    Console.WriteLine(weather.tempmax);
+                    }
+                Console.WriteLine();
+                foreach (XmlNode node2 in dayNode)
+                {
+                    Weather weather;
+                    weather = new Weather();
+                    weather.name = node2["name"].InnerText;
+                    weather.phenomenon = node2["phenomenon"].InnerText;
+                    weather.tempmin = node2["tempmin"].InnerText;
+
+
+
+                    weatherlist.Add(weather);
+                    Console.WriteLine(weather.name);
+                    Console.WriteLine(weather.phenomenon);
+                    Console.WriteLine(weather.tempmin);
+                    
+                }
+            }
+
+
+                Console.ReadKey();
+            }
+
+        }
+    
+}
---
+++ Test1/Test1/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Weather
+    {
+        public string name { get; set; }
+        public string phenomenon { get; set; }
+        public string tempmin { get; set; }
+        public string tempmax { get; set; }
+        
+
+    }
+}
